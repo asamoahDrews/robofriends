0332bed64171a47e51039ca95559fc890fbef533
Asamoah Drews <asamoahdrews@gmail.com>
2020-08-10 00:20:30 +0000

    changed the proxy request to HTTPs

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("http://jsonplaceholder.typicode.com/users")
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((respnse) => {
         return respnse.json();
       })
